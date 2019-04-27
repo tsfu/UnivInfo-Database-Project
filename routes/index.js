20bb9a0abcce729edbc8bab98935bd963a59f40b
JaniceGJY <gjy.janicecarol@gmail.com>
2019-04-27 16:00:35 -0400

    index

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,71 @@ var connection = oracledb.createConnection({
 
 
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../', 'views', 'template.html'));
+  res.sendFile(path.join(__dirname, '../', 'views', 'dashboard.html'));
 });
+
+
+
 
 router.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, '../', 'views', 'login.html'));
 });
 
+
+router.get('/recommendations/:movie', function(req, res){
+ 
+  var movie = req.params.movie;
+  var query = "select distinct genre from genres g where g.movie_id=? ";
+  connection.query(query, movie, function(err, rows){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(rows.length);
+      var lim = Math.floor(10 / Math.min(rows.length, 10)); 
+      var rem = 10 - lim * rows.length;
+       var result=[];
+       var size = rows.length;
+       
+      for (var i = 0; i < Math.min(rows.length, 10); i++) {
+       if(i == rows.length - 1){
+          lim = rem + lim;
+       }
+       var list=[movie, rows[i].genre, lim];
+       console.log(list);
+        var query2 = "select title, genre from movies m, genres g where m.id=g.movie_id and m.id <> ? and g.genre = ? order by rand() limit ?";
+        connection.query(query2, list, function(err, rows2, fields) {
+               if (err) {
+                 console.log( err );
+               }
+                 for(var x in rows2){
+                  result.push(rows2[x]);
+                 }
+                 //push query output to this variable
+                 if(0 === --size){
+                    res.json(result);
+                 }               
+           });
+      }   
+
+    }
+
+  });
+
+});
+
+router.get('/dashboard/:school', function(req, res){
+  var school = req.params.school;
+  var query = 'SELECT from University';
+  connection.query(query, function(err, rows) {
+    console.log("rows="+rows+"!!!");
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+     
+      }
+  });
+});
 // To add a new page, use the templete below
 /*
 router.get('/routeName', function(req, res) {
