385bada00b02d34672ddcd05a3c22843a9e6b918
JaniceGJY <gjy.janicecarol@gmail.com>
2019-04-29 18:55:30 -0400

    latest version

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,103 +1,117 @@
+
 var app = angular.module('angularjsNodejsTutorial', []);
 
-
-
-// app.factory('myService', function() {
-//  var savedData = {}
-//  function set(data) {
-//    savedData = data;
-//  }
-//  function get() {
-//   return savedData;
-//  }
-
-//  return {
-//   set: set,
-//   get: get
-//  }
-
-// });
-
-// Implement this controller for every HTML body!
-
-/* app.controller('TemplateController', function($scope, $http) {  
-=======
-app.controller('AdvSearchController', function($scope, $http) {  
->>>>>>> 0f068f1a0c4d2c7c0ff7ef2b89ea5c271de387ac
-  
-  $scope.users = {};
-  $scope.message="";
-  
-  var qstr = "SELECT * FROM (university U JOIN location L) JOIN award_tuition T) JOIN rank R"
-  var str1 = "WHERE U.control = '" + control + "'";
-  var str2 = "WHERE L.state = '" + state + "'";
-  var str3 = "WHERE T.tuition < " + tuition ;
-  var str4 = "WHERE R.ranking < " + ranking + "LIMIT 5;";
-
-  var query = qstr + str1 + str2 + str3 + str4;
-
-
-
-
-  var request = $http({
-    url: 'INSERT URL HERE',
-    method: "GET",
-    data: {}
-  });
-
-  request.success(function(response){
-    $scope.dummy = response;
-  });
-  
-  request.error(function(err) {
-    console.log("error: ", err);
-  });
-
-}); */
-
-app.controller('submitSearchController', function($scope, $http, $rootScope) {
-
+app.controller('submitSearchController', function($scope, $http, $window) {
   $scope.beginSearch = function() {
-    // To check in the console if the variables are correctly storing the input:
-    // console.log($scope.username, $scope.password);
-
-
-     var request = $http.get('http://localhost:8081/dashboard/'+$scope.school);
-    request.success(function(data){
-    console.log(data+"log!!!!log");
-
-    $rootScope.school=$scope.school;
-   
-    //console.log($rootScope.data+"!!!!!!!!!!!!scope");
-    window.location.pathname='../views/results.html';
-    window.location.href = "http://localhost:8081/results/"+$scope.school;
-
-    // $scope.data=data;
-
-  });
-  request.error(function(data, status){
-    console.log('err', data, status);
-  });
+    
+    console.log("school input is: " + $scope.school);
+    var request = $http.get('http://localhost:8081/showResults/' + $scope.school);   
+    
+    request.success(function(response){
+    $window.localStorage.setItem("rows", JSON.stringify(response));
+    window.location.href = "http://localhost:8081/results";      
+    });
+  
+    request.error(function(data, status){
+      console.log('err', data, status);
+    });
   }
 });
 
+app.controller('searchController', function($scope, $http, $window) {
+    $scope.shared = JSON.parse($window.localStorage.getItem("rows"));
 
-app.controller('searchController', function($scope, $http, $rootScope) {
+    console.log("Service: " + $scope.shared);
+    $scope.data = $scope.shared;
+    console.log($scope.data);
+
+  // var request = $http.get('/showResults/' + $scope.school);
+  // request.success(function(response){
+  //   console.log("SCOPE SENT!!!!!!!!!!!!!!!!!");
+  //   console.log(response + "!!!!!!!!!!!!DATA GOTTTT!!!!");
+  //   $scope.data = response;
+  // });
+
+  // request.error(function(data, status){
+  //   console.log('err', data, status);
+  // });
+});
+
+
+
+app.controller('profileController', function($scope, $http) {
   // normal variables
-
-    var request = $http.get('http://localhost:8081/results/'+$rootScope.school);
+  $scope.submit = function(){
+   var request = $http.get('http://localhost:8081/?uid='+$scope.school);
   request.success(function(data){
-    console.log($rootScope.school+"rootScope!!!!!!!!!!!!!!!!!");
-    //console.log(myService.get()+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!myService");
-    console.log(data+"!!!!!!!!!!!!scope");
+    console.log(data);
      $scope.data=data;
   });
   request.error(function(data, status){
     console.log('err', data, status);
   });
-  
+  }
   
 });
+
+
+
+
+// Template for adding a controller
+/*
+app.controller('dummyController', function($scope, $http) {
+  // normal variables
+  var dummyVar1 = 'abc';
+
+  // Angular scope variables
+  $scope.dummyVar2 = 'abc';
+
+  // Angular function
+  $scope.dummyFunction = function() {
+
+  };
+});
+*/
+"use strict";
+var app = angular.module('angularjsNodejsTutorial', []);
+
+app.controller('submitSearchController', function($scope, $http, $window) {
+  $scope.beginSearch = function() {
+    
+    console.log("school input is: " + $scope.school);
+    var request = $http.get('http://localhost:8081/showResults/' + $scope.school);   
+    
+    request.success(function(response){
+    $window.localStorage.setItem("rows", JSON.stringify(response));
+    window.location.href = "http://localhost:8081/results";      
+    });
+  
+    request.error(function(data, status){
+      console.log('err', data, status);
+    });
+  }
+});
+
+app.controller('searchController', function($scope, $http, $window) {
+    $scope.shared = JSON.parse($window.localStorage.getItem("rows"));
+
+    console.log("Service: " + $scope.shared);
+    $scope.data = $scope.shared;
+    console.log($scope.data);
+
+  // var request = $http.get('/showResults/' + $scope.school);
+  // request.success(function(response){
+  //   console.log("SCOPE SENT!!!!!!!!!!!!!!!!!");
+  //   console.log(response + "!!!!!!!!!!!!DATA GOTTTT!!!!");
+  //   $scope.data = response;
+  // });
+
+  // request.error(function(data, status){
+  //   console.log('err', data, status);
+  // });
+});
+
+
 
 app.controller('profileController', function($scope, $http) {
   // normal variables
